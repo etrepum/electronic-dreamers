30b188445994dc4f3c7b0d4bca1de36cefbfd482
Epicyoshi7 <epicyoshigreen2@gmail.com>
2014-03-13 18:02:30 -0700

    Progress 1

---
+++ greenflyer/js/screens/play.js
@@ -0,0 +1,32 @@
+/*jslint sloppy:true, browser: true, devel: true, eqeq: true, vars: true, white: true*/
+/*global game: true, debugPanel:true, me:true*/
+
+game.PlayScreen = me.ScreenObject.extend({
+	/**
+	 *  action to perform on state change
+	 */
+	onResetEvent: function() {
+        // load a level
+		me.levelDirector.loadLevel("2dshot");
+        
+		// reset the score
+		game.data.score = 0;
+
+		// add our HUD to the game world
+		this.HUD = new game.HUD.Container();
+		me.game.world.addChild(this.HUD);
+        // play the audio track
+        //me.audio.playTrack("DST-InertExponent");
+	},
+
+
+	/**
+	 *  action to perform when leaving this screen (state change)
+	 */
+	onDestroyEvent: function() {
+        // stop the current audio track
+        //me.audio.stopTrack();
+		// remove the HUD from the game world
+		me.game.world.removeChild(this.HUD);
+	}
+});
